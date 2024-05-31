36c75786c1426db82e0723564d22d467e6797ab5
Marlon Rugama <mrugama@gmail.com>
2024-05-31 10:05:52 -0400

    Genesis pr

---
+++ Nasa/Nasa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nasa
+//
+//  Created by Marlon Rugama on 5/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
